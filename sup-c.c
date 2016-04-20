d669bc3ae1ef376a3252ecd27bf5db3696c73f8f
Piotr Walczak <P.Walczak@adbglobal.com>
2016-04-20 17:29:50 +0200

    patch-test

--- sup-c.c
+++ sup-c.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
     printf("\n");
-    printf("hello yocto\n");
+    printf("Hello Yocto\n");
     printf("devtool test on poky 2.0.1\n");
     printf("\n");
     return 0;
