427248af407caf554585728da02581deda7f1ee3
Piotr Walczak <P.Walczak@adbglobal.com>
2016-04-21 12:54:22 +0200

    info about git repo

--- sup-c.c
+++ sup-c.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
     printf("\n");
     printf("Hello Yocto\n");
     printf("devtool test on poky 2.0.1\n");
+    printf("sources from sup-c.git");
     printf("\n");
     return 0;
 }
