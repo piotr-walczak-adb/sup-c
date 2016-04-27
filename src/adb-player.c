da9d44e49cdcf6cf0e3533485cef3c7fca22c9e6
Piotr Walczak <P.Walczak@adbglobal.com>
2016-04-27 17:36:33 +0200

    adding modified src

---
+++ src/adb-player.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    printf("\n");
+    printf("Hello Yocto\n");
+    printf("devtool test on poky 2.0.1\n");
+    printf("sources from sup-c.git");
+    printf("\n");
+    return 0;
+}
