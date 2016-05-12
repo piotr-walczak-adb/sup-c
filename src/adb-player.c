cc81fd1e7b11980d8ed9cef12a46b5b248921bf2
Piotr Walczak <P.Walczak@adbglobal.com>
2016-05-12 16:24:38 +0200

    Update source to mongoose test server

--- src/adb-player.c
+++ src/adb-player.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-    printf("\n");
-    printf("Hello Yocto\n");
-    printf("devtool test on poky 2.0.1\n");
-    printf("sources from sup-c.git");
-    printf("\n");
-    return 0;
+#include "mongoose.h"  // Include Mongoose API definitions
+
+// Define an event handler function
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
+  struct mbuf *io = &nc->recv_mbuf;
+
+  switch (ev) {
+    case MG_EV_RECV:
+      // This event handler implements simple TCP echo server
+      mg_send(nc, io->buf, io->len);  // Echo received data back
+      mbuf_remove(io, io->len);      // Discard data from recv buffer
+      break;
+    default:
+      break;
+  }
+}
+
+int main(void) {
+  struct mg_mgr mgr;
+
+  mg_mgr_init(&mgr, NULL);  // Initialize event manager object
+
+  // Note that many connections can be added to a single event manager
+  // Connections can be created at any point, e.g. in event handler function
+  mg_bind(&mgr, "1234", ev_handler);  // Create listening connection and add it to the event manager
+
+  for (;;) {  // Start infinite event loop
+    mg_mgr_poll(&mgr, 1000);
+  }
+
+  mg_mgr_free(&mgr);
+  return 0;
 }
